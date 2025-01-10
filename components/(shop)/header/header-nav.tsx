220f00191f540424511171881f8278707af826ce
xuanquoche <nguyentrinhxuanquoc2003@gmail.com>
2025-01-10 22:50:16 +0700

    style: update style header shop

--- components/(shop)/header/header-nav.tsx
+++ components/(shop)/header/header-nav.tsx
@@ -1,24 +1,24 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 interface HeaderNavItem {
-  title: string;
-  url: string;
+    title: string;
+    url: string;
 }
 
 interface HeaderNavProps {
-  navs: HeaderNavItem[];
+    navs: HeaderNavItem[];
 }
 
 const HeaderNav = ({ navs }: HeaderNavProps) => {
-  return (
-    <div className="flex w-[30%] justify-around">
-      {navs.map((item, index) => (
-        <Link key={index} href={item.url} className="py-1 px-2">
-          <p className="text-base font-medium">{item.title}</p>
-        </Link>
-      ))}
-    </div>
-  );
+    return (
+        <div className="flex w-[30%] justify-around">
+            {navs.map((item, index) => (
+                <Link key={index} href={item.url} className="py-1 px-2 hover:text-button-background">
+                    <p className="text-base font-medium">{item.title}</p>
+                </Link>
+            ))}
+        </div>
+    );
 };
 
 export default HeaderNav;
--- components/(shop)/header/header.tsx
+++ components/(shop)/header/header.tsx
@@ -1,27 +1,31 @@
-import LogoIcon from "@/public/icons/logo-icon.png";
-import Image from "next/image";
-import HeaderNav from "./header-nav";
-import { HEADER_NAV, HEADER_SHOP_ACTION } from "@/constants/client-links";
-import { HeaderAction } from "@/components/(admin)/header/header-action";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import HeaderSide from "./header-side";
+import LogoIcon from '@/public/icons/logo-icon.png';
+import Image from 'next/image';
+import HeaderNav from './header-nav';
+import { HEADER_NAV, HEADER_SHOP_ACTION } from '@/constants/client-links';
+import { HeaderAction } from '@/components/(admin)/header/header-action';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import HeaderSide from './header-side';
+import Link from 'next/link';
+import { ROUTES } from '@/constants';
 
 const HeaderShop = () => {
-  return (
-    <header className="py-2 px-6 sticky top-0 z-50 left-0 right-0 bg-button-background md:bg-white ">
-      <div className="md:hidden">
-        <SidebarTrigger />
-        <div>
-          <HeaderSide />
-        </div>
-      </div>
-      <div className="hidden md:flex items-center justify-between">
-        <Image src={LogoIcon} alt="logo" />
-        <HeaderNav navs={HEADER_NAV} />
-        <HeaderAction actions={HEADER_SHOP_ACTION} />
-      </div>
-    </header>
-  );
+    return (
+        <header className="py-2 px-6 sticky top-0 z-50 left-0 right-0 bg-button-background md:bg-white">
+            <div className="md:hidden">
+                <SidebarTrigger />
+                <div>
+                    <HeaderSide />
+                </div>
+            </div>
+            <div className="hidden md:flex items-center justify-between">
+                <Link href={ROUTES.HOME}>
+                    <Image src={LogoIcon} alt="logo" />
+                </Link>
+                <HeaderNav navs={HEADER_NAV} />
+                <HeaderAction actions={HEADER_SHOP_ACTION} />
+            </div>
+        </header>
+    );
 };
 
 export default HeaderShop;
